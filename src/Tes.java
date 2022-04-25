e5655442c956a7f6491a67441fffc7de6ad63bc0
顾文杰 <1604823229@qq.com>
2022-04-25 23:18:31 +0800

    a

--- src/Tes.java
+++ src/Tes.java
@@ -7,5 +7,6 @@ public class Tes {
         System.out.println("master");
         System.out.println("hot-fix1");
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
